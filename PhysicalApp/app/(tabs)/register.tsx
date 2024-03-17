a0135291350fc638a0c1283a39b941f45fc9fd3c
Khanh Huy Nguyen <huy2272@gmail.com>
2024-03-17 19:34:25 -0400

    Implemented Log in page

--- PhysicalApp/app/(tabs)/register.tsx
+++ PhysicalApp/app/(tabs)/register.tsx
@@ -1,8 +1,9 @@
+import { StyleSheet } from "react-native";
+
 import { Text, View } from "@/components/Themed";
-import LogInForm from "@/components/LogInForm";
 import { FONT, SIZES } from "../../constants";
 
-export default function TabOneScreen() {
+export default function registerScreen() {
   return (
     <View
       style={{
@@ -21,7 +22,23 @@ export default function TabOneScreen() {
           width: "80%",
         }}
       />
-      <LogInForm />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
