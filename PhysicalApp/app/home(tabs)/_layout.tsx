27ded4b288a2c55f1331959a543f96386131f9dd
Khanh Huy Nguyen <huy2272@gmail.com>
2024-04-15 03:37:02 -0400

    Implement dynamic rounting in screen.tabs

--- PhysicalApp/app/home(tabs)/_layout.tsx
+++ PhysicalApp/app/home(tabs)/_layout.tsx
@@ -1,83 +1,109 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { Link, Tabs } from "expo-router";
-import { Pressable } from "react-native";
-
+import { Tabs, router } from "expo-router";
 import Colors from "@/constants/Colors";
 import { useColorScheme } from "@/components/useColorScheme";
-import { useClientOnlyValue } from "@/components/useClientOnlyValue";
-import welcomeScreen from "@/app/(tabs)/welcome";
-import {COLORS} from "@/constants";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 function TabBarIcon(props: {
-    name: React.ComponentProps<typeof FontAwesome>["name"];
-    color: string;
+  name: React.ComponentProps<typeof FontAwesome>["name"];
+  color: string;
 }) {
-    return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
 export default function TabLayout() {
-    const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme();
+  const [email, setEmail] = useState<string>();
+  useEffect(() => {
+    let mounted = true;
+    AsyncStorage.getItem("email").then((data) => {
+      if (mounted) {
+        if (data) {
+          setEmail(data);
+        } else {
+          setEmail("");
+        }
+      }
+    });
 
-    return (
-        <Tabs
-            screenOptions={{
-                tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-                // Disable the static render of the header on web
-                // to prevent a hydration error in React Navigation v6.
-                headerShown: false,
-            }}
-        >
+    return () => {
+      mounted = false;
+    };
+  }, []);
 
-            <Tabs.Screen
-                name="Home"
-                options={{
-                    title: "Home",
-                    tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
-                }}
-            />
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        // Disable the static render of the header on web
+        // to prevent a hydration error in React Navigation v6.
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="Home"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+        }}
+      ></Tabs.Screen>
 
-            <Tabs.Screen
-                name="Explore"
-                options={{
-                    title: "Explore",
-                    tabBarIcon: ({ color }) => <TabBarIcon name="globe" color={color} />,
-                }}
-            />
+      <Tabs.Screen
+        name="Explore"
+        options={{
+          title: "Explore",
+          tabBarIcon: ({ color }) => <TabBarIcon name="globe" color={color} />,
+        }}
+      />
 
-            <Tabs.Screen
-                name="MyExercises"
-                options={{
-                    title: "My Exercises",
-                    tabBarIcon: ({ color }) => <TabBarIcon name="bicycle" color={color} />,
-                }}
-            />
+      <Tabs.Screen
+        name="MyExercises/[profile]"
+        options={{
+          title: "My Exercises",
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="bicycle" color={color} />
+          ),
+          href: {
+            pathname: `MyExercises/[profile]`,
+            params: {
+              profile: `${email}`,
+            },
+          },
+        }}
+      />
 
-            <Tabs.Screen
-                name="Rewards"
-                options={{
-                    title: "Rewards",
-                    tabBarIcon: ({ color }) => <TabBarIcon name="gift" color={color} />,
-                }}
-            />
+      <Tabs.Screen
+        name="Rewards"
+        options={{
+          title: "Rewards",
+          tabBarIcon: ({ color }) => <TabBarIcon name="gift" color={color} />,
+        }}
+      />
 
-            <Tabs.Screen
-                name="Forum"
-                options={{
-                    title: "Forum",
-                    tabBarIcon: ({ color }) => <TabBarIcon name="comments" color={color} />,
-                }}
-            />
+      <Tabs.Screen
+        name="Forum"
+        options={{
+          title: "Forum",
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="comments" color={color} />
+          ),
+        }}
+      />
 
-            <Tabs.Screen
-                name="Profile"
-                options={{
-                    title: "Profile",
-                    tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
-                }}
-            />
-        </Tabs>
-
-
-    );
+      <Tabs.Screen
+        name="Profile/[profile]"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
+          href: {
+            pathname: `Profile/[profile]`,
+            params: {
+              profile: `${email}`,
+            },
+          },
+        }}
+      />
+    </Tabs>
+  );
 }
