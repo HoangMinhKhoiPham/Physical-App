3160aabdf7c95a38fca4ed84145d3a494b995522
Khanh Huy Nguyen <huy2272@gmail.com>
2024-04-15 15:05:45 -0400

    Abstract RecentActivity to take in dynamic data

--- PhysicalApp/components/RecentActivity.tsx
+++ PhysicalApp/components/RecentActivity.tsx
@@ -1,74 +1,85 @@
-import React from 'react';
-import {View, Text, StyleSheet, Image} from 'react-native';
-import {FONT, SIZES} from "@/constants";
-import {COLORS} from "@/constants";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { FONT, SIZES } from "@/constants";
+import { COLORS } from "@/constants";
 import ProgressBar from "@/components/ProgressBar";
+import { Exercises } from "@/constants/UserProfiles";
 
-const RecentActivity = () => {
-    return (
-        <View style={styles.container}>
-            <View style={styles.topRow}>
-                <Text style={styles.exerciseTitle}>Home Workout</Text>
-                <View style={styles.level}>
-                    <Text style={styles.levelText}>Beginner</Text>
-                </View>
-            </View>
-            <View style={styles.bottomRow}>
-                <ProgressBar label={"Continue"} progress={75}/>
-            </View>
-        </View>
-    );
+interface RecentActivityProps {
+  exercise: Exercises | undefined;
 }
+const RecentActivity = (props: RecentActivityProps) => {
+  const { exercise } = props;
+  return (
+    <View style={styles.container}>
+      <View style={styles.topRow}>
+        <Text style={styles.exerciseTitle}>
+          {exercise?.title ? exercise?.title : "Home Workout"}
+        </Text>
+        <View style={styles.level}>
+          <Text style={styles.levelText}>
+            {exercise?.level ? exercise.level : "Beginner"}
+          </Text>
+        </View>
+      </View>
+      <View style={styles.bottomRow}>
+        <ProgressBar
+          label={"Continue"}
+          progress={exercise?.progress ? exercise.progress : 75}
+        />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'column',
-        gap: -50,
+  container: {
+    flexDirection: "column",
+    gap: -50,
+  },
+  topRow: {
+    width: 380,
+    height: 85,
+    backgroundColor: COLORS.secondary,
+    borderRadius: 22,
+    paddingHorizontal: 15,
+    paddingVertical: 8,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    shadowColor: "black",
+    shadowRadius: 12,
+    shadowOpacity: 1,
+    elevation: 5,
+    shadowOffset: {
+      width: 0,
+      height: 4,
     },
-    topRow: {
-        width: 380,
-        height: 85,
-        backgroundColor: COLORS.secondary,
-        borderRadius: 22,
-        paddingHorizontal: 15,
-        paddingVertical: 8,
-        flexDirection: 'row',
-        justifyContent: "space-between",
-        shadowColor: 'black',
-        shadowRadius: 12,
-        shadowOpacity: 1,
-        elevation: 5,
-        shadowOffset:
-            {
-                width: 0,
-                height: 4
-            }
-    },
-    bottomRow: {
-        width: '40%',
-        paddingVertical: 8,
-        paddingHorizontal: 15,
-    },
-    exerciseTitle: {
-        fontFamily: FONT.bold,
-        fontSize: SIZES.large,
-        color: COLORS.text,
-    },
-    level: {
-        width: 115,
-        height: 30,
-        backgroundColor: 'rgba(89, 126, 255, 0.75)',
-        borderRadius: 22,
-        borderWidth: 1,
-        borderColor: 'black',
-        borderStyle: 'solid',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    levelText: {
-        color: 'white',
-        fontFamily: FONT.regular,
-    },
+  },
+  bottomRow: {
+    width: "40%",
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+  },
+  exerciseTitle: {
+    fontFamily: FONT.bold,
+    fontSize: SIZES.large,
+    color: COLORS.text,
+  },
+  level: {
+    width: 115,
+    height: 30,
+    backgroundColor: "rgba(89, 126, 255, 0.75)",
+    borderRadius: 22,
+    borderWidth: 1,
+    borderColor: "black",
+    borderStyle: "solid",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  levelText: {
+    color: "white",
+    fontFamily: FONT.regular,
+  },
 });
 
 export default RecentActivity;
